2b06d2b6806fd460c0815af56801d951f50f69b7
Daniyar <ddr199305@gmail.com>
2018-10-12 15:37:43 +0200

    Change description

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Example React.js SPA deployed to AWS using Docker and Travis-CI</p>
           <a
             className="App-link"
             href="https://reactjs.org"
